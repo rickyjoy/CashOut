2bc526a3bcb8bb270b803871d2cbcdec2df81171
rickyjoy <rickyjoy@live.com>
2012-10-15 18:43:23 -0700

    Initial Commit

---
+++ CashOut/BIDAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BIDAppDelegate.h
+//  CashOut
+//
+//  Created by Rick on 10/15/12.
+//  Copyright (c) 2012 Rick. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BIDAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ CashOut/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  CashOut
+//
+//  Created by Rick on 10/15/12.
+//  Copyright (c) 2012 Rick. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "BIDAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([BIDAppDelegate class]));
+    }
+}
